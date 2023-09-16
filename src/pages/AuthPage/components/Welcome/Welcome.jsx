070d1df2101afab21a339976410a363982c43bee
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2023-09-16 21:20:37 +0600

    fix welcome height

--- src/pages/AuthPage/components/Welcome/Welcome.jsx
+++ src/pages/AuthPage/components/Welcome/Welcome.jsx
@@ -3,13 +3,14 @@ import React from "react";
 import welcomeImage from "../../../../assets/welcome.gif";
 export const Welcome = () => {
   const matches_md = useMediaQuery("(min-width: 980px)");
+  const matches_xs = useMediaQuery("(min-width: 560px)");
 
   return (
     <Box>
       <img
         src={welcomeImage}
         width="100%"
-        height={matches_md ? "700px" : "450px"}
+        height={matches_md ? "700px" : matches_xs ?  "450px" : "250"}
         alt=""
       />
     </Box>
